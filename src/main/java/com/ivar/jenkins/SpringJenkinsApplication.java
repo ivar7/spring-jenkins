dbb5b1d31e46b060b56938f4d447e932e5ccd67c
unknown <gowdravi98@gmail.com>
2022-05-30 12:07:25 +0530

    first commit

---
+++ src/main/java/com/ivar/jenkins/SpringJenkinsApplication.java
@@ -0,0 +1,27 @@
+package com.ivar.jenkins;
+
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application Started... ");
+	}
+	
+	public static void main(String[] args) {
+		logger.info("Main Application Started... ");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
